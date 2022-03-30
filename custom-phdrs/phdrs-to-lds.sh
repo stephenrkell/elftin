65d87ba1207e3f6893f1b80c8c7c6c86f7831f27
Stephen Kell <srk31@srcf.ucam.org>
2022-03-30 21:21:20 +0100

    custom-phdrs: when inserting the PHDRS keyword into the linker script, don't require first LOAD to be executable

--- custom-phdrs/phdrs-to-lds.sh
+++ custom-phdrs/phdrs-to-lds.sh
@@ -3,15 +3,16 @@
 readelf -Wl "$1" | \
 sed -ne '/^Program Headers/,/^ Section to Segment mapping/ p' | grep -v '[.*]' | \
 tail -n+3 | head -n-2 | sed 's/^[[:blank:]]*//' | tr -s '[:blank:]' '\t' | \
-(ctr=0; while read pt_type pt_offset pt_vaddr pt_paddr pt_filesz pt_memsz flags_and_align; do \
-	align=$( echo "$flags_and_align" | sed 's/.*\(0x.*\)/\1/' ); \
-	flags=$( echo "$flags_and_align" | sed 's/\(.*\)0x.*/\1/' | tr -cd RWE ); \
-	if [[ $pt_type == "PHDR" ]] || ( \
-	[[ $pt_type == "LOAD" ]] && \
-	grep 'E' <<<"$flags" >/dev/null && \
-	[[ $(( $pt_offset )) -lt $(( 0x1000 )) ]] ); then \
-		phdr_keyword="PHDRS"; else phdr_keyword=""; \
-	fi; \
+(ctr=0; while read pt_type pt_offset pt_vaddr pt_paddr pt_filesz pt_memsz flags_and_align; do
+	align=$( echo "$flags_and_align" | sed 's/.*\(0x.*\)/\1/' )
+	flags=$( echo "$flags_and_align" | sed 's/\(.*\)0x.*/\1/' | tr -cd RWE )
+    echo "flags for type $pt_type offset $pt_offset is $flags" 1>&2
+	if [[ $pt_type == "PHDR" ]] || (
+	[[ $pt_type == "LOAD" ]] &&
+	grep '[RE]' <<<"$flags" >/dev/null &&
+	[[ $(( $pt_offset )) -lt $(( 0x1000 )) ]] ); then
+		phdr_keyword="PHDRS"; else phdr_keyword=""
+	fi;
 	echo "phdr$ctr PT_$pt_type $phdr_keyword; " \
 ; ctr=$(( $ctr + 1 )); done) | cpp -P -imacros elf.h | sed '/^[[:blank:]]*$/ d'
 
